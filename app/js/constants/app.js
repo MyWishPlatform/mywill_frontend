66af6dad397b0b4d2045f3d5a167362baa470059
dimanok <kovalyov1987@gmail.com>
2018-08-22 19:05:14 +0300

    Fixed eos price icon

--- app/js/constants/app.js
+++ app/js/constants/app.js
@@ -54,6 +54,7 @@ module.constant('APP_CONSTANTS', {
         "contracts":0,
         "is_ghost":true,
         "balance":"0",
+        "eos_balance":"0",
         "visibleBalance":"0",
         "internal_btc_address":null,
         "use_totp":false,

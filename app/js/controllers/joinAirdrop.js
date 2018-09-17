742328b28bf0a2e0e02952c42ce78efdcca39aaf
dimanok <kovalyov1987@gmail.com>
2018-09-17 18:21:50 +0300

    Added EOS address checker to Airdrop Tool

--- app/js/controllers/joinAirdrop.js
+++ app/js/controllers/joinAirdrop.js
@@ -1,6 +1,54 @@
-angular.module('app').controller('joinAirdropController', function($scope, $timeout, EOSService) {
+angular.module('app').controller('joinAirdropController', function($scope, $timeout, EOSService, web3Service, AIRDROP_TOOL) {
 
     var checkAddressTimeout;
+    $scope.checkedEthAddress = false;
+
+    var isProduction = (location.host.indexOf('eos.mywish.io') === 0) || (location.host.indexOf('contracts.mywish.io') > -1);
+    $scope.network = isProduction ? 1 : 2;
+    web3Service.setProviderByNumber($scope.network);
+
+    var web3 = web3Service.web3();
+
+    var getEOSAddress = function(addressField) {
+        var address = Web3.utils.toChecksumAddress(addressField.$viewValue);
+        var enteredAddress = addressField.$viewValue;
+        var contract = web3Service.createContractFromAbi(AIRDROP_TOOL.CONTRACT_ADDRESS, AIRDROP_TOOL.ABI);
+        contract.methods.get(address).call(function(error, response) {
+            if (error || (enteredAddress != addressField.$viewValue)) {
+                $scope.balanceInProgress = false;
+                $scope.$apply();
+                return;
+            }
+            $scope.checkedEOSAccount = response;
+            $scope.$apply();
+        });
+    };
+
+    var checkBalanceTimer;
+    $scope.checkETHAddress = function(addressField) {
+        $scope.checkedEthAddress = false;
+        $scope.checkedEOSAccount = false;
+        if (!addressField.$valid) return;
+        $scope.balanceInProgress = true;
+        var address = Web3.utils.toChecksumAddress(addressField.$viewValue);
+        var enteredAddress = addressField.$viewValue;
+        checkBalanceTimer ? $timeout.cancel(checkBalanceTimer) : false;
+        checkBalanceTimer = $timeout(function() {
+            getEOSAddress(addressField);
+            web3.eth.call({
+                to: "0x1b22c32cd936cb97c28c5690a0695a82abf688e6",
+                data: "0x70a08231000000000000000000000000" + address.split('x')[1]
+            }, function(error, response) {
+                $scope.balanceInProgress = false;
+                if (error || (addressField.$viewValue != enteredAddress)) {
+                    $scope.$apply();
+                    return
+                }
+                $scope.checkedEthAddress = Web3.utils.fromWei(new BigNumber(Web3.utils.hexToNumberString(response)).toString(10), 'ether');
+                $scope.$apply();
+            });
+        }, 500);
+    };
 
     $scope.checkAddress = function(addressField) {
         addressField.$setValidity('not-checked', true);
@@ -26,11 +74,10 @@ angular.module('app').controller('joinAirdropController', function($scope, $time
 
     var isProduction = (location.host.indexOf('eos.mywish.io') === 0) || (location.host.indexOf('contracts.mywish.io') > -1);
     $scope.network = isProduction ? 1 : 2;
-
     web3Service.setProviderByNumber($scope.network);
 
     var interfaceMethod = web3Service.getMethodInterface('put', AIRDROP_TOOL.ABI);
-    console.log(interfaceMethod);
+
     $scope.setAddressSignature = (new Web3()).eth.abi.encodeFunctionCall(
         interfaceMethod, [$scope.ngPopUp.params.eos_address]
     );

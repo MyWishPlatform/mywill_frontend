013eba120941efdcadac9eee156852b4da8f5dd4
dimanok87 <kovalyov1987@gmail.com>
2018-11-06 15:17:07 +0300

    Moved new texts to i18n Created forms for buy EOSISH on platform

--- app/js/controllers/eosBuytokens.js
+++ app/js/controllers/eosBuytokens.js
@@ -2,6 +2,13 @@ angular.module('app').controller('eosBuytokensController', function($scope, $tim
 
     $scope.exRate = exRate.data;
 
+    $scope.payDone = function() {
+        $state.go($rootScope.currentUser.contracts ? 'main.contracts.list' : 'main.createcontract.types');
+    };
+
+    $scope.eosAccountAddress = EOSService.getComingAddress();
+
+
     var resetForm = function() {
         $scope.formData = {
             toAddress: $rootScope.currentUser.internal_address,
@@ -12,57 +19,8 @@ angular.module('app').controller('eosBuytokensController', function($scope, $tim
 
     resetForm();
 
-    $scope.$watch('visibleForm', function() {
-        resetForm();
-    });
-
-    $scope.payDone = function() {
-        $state.go($rootScope.currentUser.contracts ? 'main.contracts.list' : 'main.createcontract.types');
-    };
-
-    var rate = 0.1;//$scope.exRate.EOS;
-
-    $scope.checkWishesAmount = function() {
-        var wishesAmount = new BigNumber($scope.formData.eosAmount || 0);
-        $scope.formData.wishesAmount  = wishesAmount.div(rate).round(2).toString(10);
-        $scope.formData.amount = $scope.formData.eosAmount;
-    };
-    $scope.checkEosAmount = function() {
-        if (!$scope.formData.wishesAmount) return;
-        var ethAmount = new BigNumber($scope.formData.wishesAmount);
-        $scope.formData.eosAmount = ethAmount.times(rate).round(2).toString(10);
-        $scope.formData.amount = $scope.formData.eosAmount;
-    };
-
-
-    $scope.closeScatterAlert = function() {
-        $scope.scatterNotInstalled = false;
-        $scope.accountNotFinded = false;
-        $scope.mintServerError = false;
-    };
-
-    $scope.wishAddress = EOSService.getComingAddress();
-    $scope.sendTransaction = function() {
-
-        $scope.scatterNotInstalled = !EOSService.checkScatter();
-        if ($scope.scatterNotInstalled) return;
-
-        EOSService.createEosChain(10, function() {
-            EOSService.buyTokens(
-                new BigNumber($scope.formData.amount).toFormat(4).toString(10),
-                $scope.currentUser.memo
-            ).then(function(result) {
-                $scope.successTransaction = result;
-            }, function(error) {
-                switch(error.code) {
-                    case 1:
-                        $scope.accountNotFinded = true;
-                        break;
-                    case 2:
-                        $scope.mintServerError = true;
-                        break;
-                }
-            });
-        });
-    };
+}).controller('eosBuytokensEosController', function($scope) {
+    $scope.formData = {};
+}).controller('eosBuytokensEosishController', function($scope) {
+    $scope.formData = {};
 });

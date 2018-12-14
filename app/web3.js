955bb78d28430b20afe9ba983abb4b2b6b32874b
dimanok87 <kovalyov1987@gmail.com>
2018-12-14 18:36:57 +0300

    Added Tron Web

--- app/web3.js
+++ app/web3.js
@@ -4,3 +4,11 @@ window.Web3 = require('web3');
 window.abi = require('human-standard-token-abi');
 window.WAValidator = require('wallet-address-validator');
 
+
+window.TronWeb = require('tronweb');
+window.TronHttpProvider = window.TronWeb.providers.HttpProvider;
+
+// const fullNode = new HttpProvider('https://api.trongrid.io'); // Full node http endpoint
+// const solidityNode = new HttpProvider('https://api.trongrid.io:'); // Solidity node http endpoint
+// const eventServer = 'https://api.trongrid.io'; // Contract events http endpoint
+

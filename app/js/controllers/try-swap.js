2e6e14656fc65b0574b49e5d4d19c385588c7f07
dimanok87 <kovalyov1987@gmail.com>
2019-09-03 14:51:32 +0300

    SWAPS widget test (orders list)

--- app/js/controllers/try-swap.js
+++ app/js/controllers/try-swap.js
@@ -1,12 +1,40 @@
 angular.module('app').controller('trySwapController', function($scope) {
 
-    $scope.initSwapsWidget = function() {
-        SwapsNetwork.init('swaps-network-widget', {
+    $scope.showedTab = 'list';
+
+    $scope.iniSwapsList = function() {
+        // SwapsNetwork.getSwapOrdersList().then(function(result) {
+        // });
+        SwapsNetwork.drawOrdersList('orders-list').then((orders) => {
+            console.log(orders);
+        });
+    };
+
+    $scope.iniSwapsForm = function() {
+        SwapsNetwork.drawForm('swaps-network-widget', {
             onSubmit: function(result) {
-                $scope.successSwapForm = true;
+                $scope.setTab('list');
+                $scope.$apply();
             }
         });
     };
 
+    $scope.swapIsInit = false;
+
+    $scope.initSwapsWidget = function() {
+        SwapsNetwork.init({
+            userId: $scope.currentUser ? $scope.currentUser.id : 'DEFAULT',
+            onError: function(error) {
+                console.log(error);
+            }
+        }).then((result) => {
+            $scope.swapIsInit = true;
+            $scope.$apply();
+        });
+    };
+
+    $scope.setTab = function(tab) {
+        $scope.showedTab = tab;
+    };
 
 });

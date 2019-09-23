edce438450aad234b4e581fe4d97c94b731907bf
dimanok87 <kovalyov1987@gmail.com>
2019-09-23 12:51:59 +0300

    SWAP tokens payment method

--- app/js/controllers/buytokens.js
+++ app/js/controllers/buytokens.js
@@ -34,14 +34,14 @@ angular.module('app').controller('buytokensController', function($scope, $timeou
     };
 
     $scope.convertAmountTo = function(toField) {
-        var rate = $scope.exRate[toField];
+        var rate = $scope.exRate[toField] || 2;
         var currencyValue = new BigNumber($scope.amountsValues.WISH || 0);
         $scope.amountsValues[toField]  = currencyValue.times(rate).round(2).toString(10);
         convertToUSDT();
     };
 
     $scope.convertAmountFrom = function(fromField) {
-        var rate = $scope.exRate[fromField];
+        var rate = $scope.exRate[fromField] || 2;
         var currencyValue = new BigNumber($scope.amountsValues[fromField] || 0);
         $scope.amountsValues.WISH  = currencyValue.div(rate).round(2).toString(10);
         convertToUSDT();
@@ -64,6 +64,10 @@ angular.module('app').controller('buytokensController', function($scope, $timeou
                 'label': 'WISH Ethereum',
                 'value': 'wish',
                 'select-icon': '/static/images/blockchain/mweth2.svg'
+            }, {
+                'label': 'SWAP',
+                'value': 'swap',
+                'select-icon': '/static/images/logos/swap-logo.svg'
             }, {
                 'label': 'BNB',
                 'value': 'bnb',
@@ -172,4 +176,48 @@ angular.module('app').controller('buytokensController', function($scope, $timeou
         }).then(console.log);
     };
 
+}).controller('buyWishByWishController', function($scope, $state, $rootScope, APP_CONSTANTS, web3Service) {
+
+    $scope.wishAddress = APP_CONSTANTS.SWAP.ADDRESS;
+
+    $scope.getProvider = function(name) {
+        web3Service.setProvider(name, 1);
+        return web3Service.web3();
+    };
+
+    web3Service.setProvider(name, 1);
+    web3Service.getAccounts(1).then(function(response) {
+        response.map(function(wallet) {
+            $scope.wallets[wallet.type].push(wallet.wallet);
+        });
+    });
+
+    $scope.$watch('amountsValues.SWAP', function() {
+        if (!$scope.amountsValues.SWAP) return;
+
+        $scope.checkedTransferData = (new Web3).eth.abi.encodeFunctionCall({
+            name: 'transfer',
+            type: 'function',
+            inputs: [{
+                type: 'address',
+                name: 'to'
+            }, {
+                type: 'uint256',
+                name: 'value'
+            }]
+        }, [
+            $scope.currentUser.internal_address,
+            new BigNumber($scope.amountsValues.SWAP).times(Math.pow(10, 18)).toString(10)
+        ]);
+    });
+
+    $scope.sendTransaction = function() {
+        $scope.getProvider($scope.formData.activeService).eth.sendTransaction({
+            from: $scope.formData.address,
+            to: APP_CONSTANTS.SWAP.ADDRESS,
+            data: $scope.checkedTransferData
+        }).then(console.log);
+    };
+
 });
+

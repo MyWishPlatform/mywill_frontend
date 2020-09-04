15ab4a0f7764b132cf728c9c170b68c9d5b2f032
dimanok87 <kovalyov1987@gmail.com>
2020-09-04 15:22:26 +0300

    Change Tron shasta API url

--- app/js/constants/tronNetworks.js
+++ app/js/constants/tronNetworks.js
@@ -2,11 +2,11 @@ var module = angular.module('Constants');
 module.constant('TRON_NETWORKS_CONSTANTS',
     {
         "TESTNET": {
-            FULL_NODE: new window.TronHttpProvider('https://trontestnet.mywish.io'),
-            SOLIDITY_NODE: new window.TronHttpProvider('https://trontestnet.mywish.io'),
+            FULL_NODE: new window.TronHttpProvider('https://api.shasta.trongrid.io'),
+            SOLIDITY_NODE: new window.TronHttpProvider('https://api.shasta.trongrid.io'),
             // EVENT_SERVER: 'https://api.shasta.trongrid.io/'
-            EVENT_SERVER: 'https://trontestnet.mywish.io/',
-            API: 'https://tronscan.mywish.io'
+            EVENT_SERVER: 'https://api.shasta.trongrid.io/',
+            API: 'https://api.shasta.trongrid.io'
         },
         "MAINNET": {
             FULL_NODE: new window.TronHttpProvider('https://api.trongrid.io'),

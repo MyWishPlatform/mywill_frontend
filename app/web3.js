0cd3c451988f1254d11006a8778f6495aa95d3fe
Hotiakov <Fauxlivegame@gmail.com>
2022-01-18 13:48:07 +0300

    valid

--- app/web3.js
+++ app/web3.js
@@ -5,4 +5,5 @@ window.WAValidator = require('wallet-address-validator');
 window.TronWeb = require('tronweb');
 window.TronHttpProvider = window.TronWeb.providers.HttpProvider;
 window.Eos = require('eosjs');
+window.NewEos = require('eosjs-ecc');
 

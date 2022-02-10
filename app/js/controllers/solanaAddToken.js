d13496dbc91610b1b0c8009c464593a9abee3ea1
Hotiakov <Fauxlivegame@gmail.com>
2022-02-10 16:48:28 +0300

    init

--- app/js/controllers/solanaAddToken.js
+++ app/js/controllers/solanaAddToken.js
@@ -1,24 +1,40 @@
-angular.module('app').controller('solanaAddTokenController', function($scope){
+angular.module('app').controller('solanaAddTokenController', function ($scope){
     $scope.isValidFile = true;
     $scope.filePlaceHolder = "Upload toke logo file";
     $scope.isFileEmpty = true;
+    var file;
     $scope.setFile = function(event){
         $scope.$apply(function(){
-            var file = event.target.files[0];
+            file = event.target.files[0];
             if (!file) {
                 $scope.isFileEmpty = true;
                 return;
             }
             $scope.isFileEmpty = false;
+            $scope.filePlaceHolder = file.name.length > 16 ? file.name.split('.')[0].slice(0, 15) + '...' + file.name.split('.')[1] : file.name;
             if (file.size > 200 * 1024 || !file.type.match(/^image/)) {
                 $scope.isValidFile = false;
-                $scope.filePlaceHolder = file.name;
                 return;
             }
             else {
                 $scope.isValidFile = true;
-                $scope.filePlaceHolder = file.name;
             }
         })
+    }
+
+    $scope.sendTokenInfo = function(){
+        var reader = new FileReader();
+        reader.onload = function(e){
+            var formData = {
+                "details": $scope.contract.contract_details,
+                "logo": e.target.result,
+                "site_link": $scope.link,
+                "coingecko_id": $scope.coingecko,
+                "description": $scope.description,
+                "disc_link": $scope.discord,
+                "twitter_link": $scope.twitter,
+            }
+        }
+        reader.readAsBinaryString(file);
     }
 });
